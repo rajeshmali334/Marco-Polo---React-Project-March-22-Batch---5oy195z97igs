d8855a861aaeacc130c987b36f61f9df2f3738af
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2022-10-10 00:47:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,14 @@ import '../styles/App.css';
 const App = () => {
   const [text, setText]=useState("Marco");
   const [buttentext, setButtenText]=useState("Polo");
-  
+  const handle=()=>{
+    setText("Polo");
+    setButtenText("Marco");
+  }
   return (
     <div id="main">
         <h1 id="marco-polo">{text}</h1>
-        <button  id="marco-polo-toggler" >{buttentext}</button>
+        <button onClick={handle}  id="marco-polo-toggler" >{buttentext}</button>
     </div>
   )
 }
