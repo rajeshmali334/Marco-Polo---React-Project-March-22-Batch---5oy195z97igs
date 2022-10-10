e218fcf69b6da30b4504a115787551fc8d1ae6e4
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2022-10-10 11:48:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  const [text, setText]=useState("Marco");
-  const [buttentext, setButtenText]=useState("Polo");
-  const handle=()=>{
-    setText("Polo");
-    setButtenText("Marco");
+  const [getData,setData]=useState("Marco")
+  const [getBtn,setBtn]=useState("Polo")  
+
+  const ChangeHandler=()=>{
+    if(getData==="Marco"){
+      setData("Polo")
+      setBtn("Marco")
+    }else if(getData ==='Polo'){
+      setData("Marco")
+      setBtn("Polo")
+    }
   }
+
   return (
     <div id="main">
-        <h1 id="marco-polo">{text}</h1>
-        <button onClick={handle}  id="marco-polo-toggler" >{buttentext}</button>
+      <h1 id="marco-polo">{getData}</h1>
+      <button id="marco-polo-toggler" onClick={ChangeHandler}>{getBtn}</button>
     </div>
   )
 }
